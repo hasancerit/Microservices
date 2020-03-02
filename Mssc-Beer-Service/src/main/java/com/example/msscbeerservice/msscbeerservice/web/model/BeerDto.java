77157d504a59833b8fd0c980f68449949b8a55e6
hasancerit <hasan__cerit@hotmail.com>
2020-03-02 03:19:43 +0300

    validation

--- Mssc-Beer-Service/src/main/java/com/example/msscbeerservice/msscbeerservice/web/model/BeerDto.java
+++ Mssc-Beer-Service/src/main/java/com/example/msscbeerservice/msscbeerservice/web/model/BeerDto.java
@@ -4,6 +4,11 @@ import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Positive;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,13 +19,27 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Builder
 public class BeerDto {
+	
+	@Null
 	private UUID id;
+	@Null
 	private Integer version;
+	@Null
 	private OffsetDateTime createdDate;
+	@Null
 	private OffsetDateTime lastModifiedDate;
+	
+	@NotBlank
 	private String beerName;
+	
+	@NotNull
 	private BeerStyleEnum beerStye;
+	
+	@Positive
 	private Long upc;
+	
+	@NotNull
+	@Positive
 	private BigDecimal price;
 	private Integer quantityOnHand;
 }
